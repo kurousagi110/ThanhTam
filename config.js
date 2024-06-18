1b6a88780c66bacee6aa03b662dad3f0c03b57ed
kurousagi110 <110773925+kurousagi110@users.noreply.github.com>
2024-06-19 01:29:45 +0700

    e

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introDesc: `Anh có điều này muốn nói với em, dù có thể là em đã biết rồi nhưng anh vẫn muốn nói`,
     btnIntro: 'Continue',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên <3',
-    desc: 'Lúc gặp em lần đầu anh đã thấy mình yêu em rồi, nhưng anh chỉ dám yêu thầm k dám nói ra. Càng về sau anh càng thấy mình yêu em nhiều hơn. Anh muốn nói với em rằng anh yêu em nhiều lắm. Không biết anh có thể được nắm tay cùng bước với em không  <3 ',
+    desc: 'Lúc gặp em lần đầu anh đã thấy mình yêu em rồi, nhưng anh chỉ dám yêu thầm k dám nói ra. Càng về sau khi trò chuyện với em nhiều hơn anh càng cảm thấy yêu em nhiều hơn. Anh thì khù khờ và có nhiều khuyết điểm nhưng anh yêu em thật lòng và hứa sẽ làm em hạnh phúc. Không biết anh có thể được nắm tay cùng bước với em không? <3 ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
